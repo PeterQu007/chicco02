8e37f0e250f01ef9c1840b04458ac40bfe9f0e72
PeterQu <peterqu007@gmail.com>
2017-07-28 11:17:12 -0700

    remove login info

--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,23 +1,3 @@
-// $(function(){
-
-    
-//     var name =$("#j_username");
-//     var pswhidden = $("#j_password");
-//     var psw = $("#password");
-//     var btn = $("#loginbtn");
-    
-//     console.log(name);
-    
-//     name.val("V70898");
-//     psw.css("color","white");
-//     psw.val("escape89");
-//     pswhidden.val("escape89");
-//     btn.click();
-
-//     $('#message').text("login to MLS");
-    
-	
-// })
 
 console.log("Popup.js");
 $('#message').text("Popup.js");
